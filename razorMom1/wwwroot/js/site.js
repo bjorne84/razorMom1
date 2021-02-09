f605be294c61f9172eaf9621da58853294927243
bjorne84 <edinbjorn@gmail.com>
2021-02-09 18:52:40 +0100

    ok nu verkar allt fungera dugligt med css:en

--- razorMom1/wwwroot/js/site.js
+++ razorMom1/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+(function () {
+
+    let hamburger = {
+        navToggle: document.querySelector('.nav-toggle'),
+        nav: document.querySelector('nav'),
+
+        doToggle: function (e) {
+            e.preventDefault();
+            this.navToggle.classList.toggle('expanded');
+            this.nav.classList.toggle('expanded');
+        }
+    };
+
+    hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
+    //tog bort nedanstående, då funkade ej länkarna
+    /*hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });*/
+
+}());
